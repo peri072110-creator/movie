f8184b2dfffef1773eb8243d0ceea6a837589a3c
agzet <agzetjik@gmail.com>
2026-01-28 10:05:59 +0600

    DONE

--- mysity/settings.py
+++ mysity/settings.py
@@ -132,6 +132,7 @@ MODELTRANSLATION_LANGUAGES = ('ru', 'en')
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
@@ -172,7 +173,7 @@ SIMPLE_JWT = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:8000",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
